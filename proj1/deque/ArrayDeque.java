478edb90b1b2d8855b4efcdcdbbe2e9182efabed
vita302 <tedlee7302@outlook.com>
2024-09-25 22:31:15 +0800

    Check Point 1

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,98 @@
+package deque;
+
+import java.util.ArrayList;
+
+class ArrayDeque<Item> {
+
+    private Item[] arr;
+    private int arr_size;
+    private int first;
+    private int last;
+    private int length;
+
+    public ArrayDeque() {
+        arr = (Item[]) new Object[8];
+        arr_size = 0;
+        length = 8;
+        first = 0;
+        last = 0;
+    }
+
+    public void resize(int cap) {
+        Item[] narr = (Item[]) new Object[cap];
+        int i = first;
+        int j = 0;
+        while (j < arr_size) {
+            narr[j] = arr[i];
+            i = (i + 1) % length;
+            j ++;
+        }
+        first = 0;
+        if (isEmpty()) last = 0;
+        else last = j - 1;
+        arr = narr;
+        length = cap;
+    }
+
+    public Item removeLast() {
+        if (isEmpty()) return null;
+        int t = (last - 1) & (length - 1);
+
+        Item x = arr[t];
+        //arr[t] = null;
+        last = t;
+        arr_size -= 1;
+        if (arr_size * 4 < length) resize(length / 2);
+        return x;
+    }
+
+    public void addLast(Item x) {
+        arr[last] = x;
+        arr_size += 1;
+        if ((last = (last + 1) & (length - 1)) == first) resize(length * 2);
+    }
+
+    public int size() {
+        return arr_size;
+    }
+
+    public boolean isEmpty() {
+        if (arr_size == 0) return true;
+        return false;
+    }
+    public void addFirst(Item x) {
+        arr[first = (first - 1) & (length - 1)] = x;
+        if (first == last) resize(length * 2);
+        arr_size += 1;
+    }
+
+    public Item removeFirst() {
+        if (isEmpty()) return null;
+        int h = first;
+        Item x = (Item) arr[h];
+        if (x == null) return null;
+
+        arr[h] = null;
+        first = (first + 1) & (length - 1);
+        if (arr_size * 4 < length) resize(length / 2);
+        return x;
+    }
+
+    public Item get(int i) {
+        if (i < 0 || i >= arr_size) return null;
+        int pos = (i + first) % length;
+        return arr[pos];
+    }
+
+    public void printDeque() {
+        int hh = first , tt = last;
+
+        while (hh < tt) {
+            Item t = arr[hh];
+            System.out.print(t);
+            System.out.print(' ');
+            hh = (hh + 1) % length;
+        }
+        System.out.println();
+    }
+}
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,119 @@
+package deque;
+
+import java.awt.event.ItemEvent;
+
+class LinkedListDeque<Item> {
+    private class Node<Item> {
+        Item value;
+        Node<Item> next;
+        Node<Item> prev;
+
+    }
+
+    private Node<Item> sentinal , endSentinal;
+    private int list_size = 0;
+
+    public LinkedListDeque() {
+        sentinal = new Node<>();
+        endSentinal = new Node<>();
+        sentinal.next = endSentinal;
+        endSentinal.prev = sentinal;
+        sentinal.value = endSentinal.value = null;
+    }
+
+    public Item getRecursive(int index) {
+        Node<Item> curr = sentinal.next;
+        if (curr == endSentinal) return null;
+        return getByR(curr , index);
+    }
+
+    private Item getByR(Node<Item> curr , int index)
+    {
+        if (index == 0) return curr.value;
+        else {
+            curr = curr.next;
+            if (curr == endSentinal) return null;
+            return getByR(curr , index - 1);
+        }
+    }
+
+
+
+    public void addFirst(Item x) {
+        Node<Item> fr = new Node<>();
+        fr.value = x;
+        fr.next = sentinal.next;
+        sentinal.next = fr;
+        fr.prev = sentinal;
+        list_size += 1;
+        if (endSentinal.prev == sentinal) endSentinal.prev = fr;
+    }
+
+    public void addLast(Item x) {
+
+        Node<Item> curr = endSentinal.prev;
+        Node<Item> fr = new Node<>();
+        fr.value = x;
+        fr.next = endSentinal;
+        curr.next = fr;
+        fr.prev = curr;
+        endSentinal.prev = fr;
+        if (sentinal.next == endSentinal) sentinal.next = fr;
+        list_size += 1;
+    }
+
+    public boolean isEmpty() {
+        if (list_size != 0) return false;
+        return true;
+    }
+
+    public int size() {
+        return list_size;
+    }
+
+    public void printDeque() {
+        Node<Item> pointer;
+        pointer = sentinal.next;
+        while (pointer != endSentinal) {
+            System.out.print(pointer.value);
+            System.out.print(' ');
+            pointer = pointer.next;
+        }
+
+        System.out.println();
+    }
+
+    public Item removeFirst() {
+        Node<Item> curr = sentinal.next;
+        if (curr == endSentinal) return null;
+        Item x = sentinal.next.value;
+        curr.prev = sentinal;
+        sentinal.next = curr.next;
+        list_size -= 1;
+        return x;
+    }
+
+    public Item removeLast() {
+        Node<Item> curr = endSentinal.prev;
+        if (curr == sentinal) return null;
+        Item x = curr.value;
+        curr.prev.next = endSentinal;
+        endSentinal.prev = curr.prev;
+        list_size -= 1;
+        return x;
+    }
+
+    public Item get(int index) {
+        int count = 0;
+        Node<Item> curr = sentinal.next;
+
+        while (count != index) {
+            curr = curr.next;
+            count += 1;
+            if (curr == endSentinal) return null;
+        }
+
+        return curr.value;
+    }
+
+}
