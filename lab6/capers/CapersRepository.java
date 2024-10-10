edc560eb06ac18f7290177be77d7761eda5979dc
vita302 <tedlee7302@outlook.com>
2024-10-10 12:03:13 +0800

    Lab6 Done

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -18,7 +18,7 @@ public class CapersRepository {
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
+    static final File CAPERS_FOLDER = Utils.join(CWD,".capers"); // TODO Hint: look at the `join`
                                             //      function in Utils
 
     /**
@@ -32,6 +32,9 @@ public class CapersRepository {
      */
     public static void setupPersistence() {
         // TODO
+        CAPERS_FOLDER.mkdir();
+        final File DOGS_FOLDER = Utils.join(CWD ,".capers" , "dog");
+        DOGS_FOLDER.mkdir();
     }
 
     /**
@@ -41,6 +44,16 @@ public class CapersRepository {
      */
     public static void writeStory(String text) {
         // TODO
+        File target = Utils.join(CWD , ".capers" , "story");
+        if (target.exists()) {
+            String old = readContentsAsString(target);
+            writeContents(target , old ,"\n" , text );
+        } else {
+            writeContents(target , text );
+        }
+
+        String nw = readContentsAsString(target);
+        System.out.print(nw);
     }
 
     /**
@@ -50,6 +63,9 @@ public class CapersRepository {
      */
     public static void makeDog(String name, String breed, int age) {
         // TODO
+        Dog d = new Dog(name , breed , age);
+        d.saveDog();
+        System.out.println(d.toString());
     }
 
     /**
@@ -60,5 +76,8 @@ public class CapersRepository {
      */
     public static void celebrateBirthday(String name) {
         // TODO
+        Dog d = Dog.fromFile(name);
+        d.haveBirthday();
+        writeObject(Utils.join(Dog.DOG_FOLDER , name) , d);
     }
 }
---
+++ lab6/capers/testStory.java
@@ -0,0 +1,36 @@
+package capers;
+
+/*
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class testStory {
+
+    @Test
+    public void TestStory() {
+        String text = "Test one two three";
+        System.out.println(CapersRepository.CWD);
+        CapersRepository.setupPersistence();
+        CapersRepository.writeStory(text);
+        CapersRepository.writeStory("alpha 1 2 3 4");
+    }
+
+    @Test
+    public void testBirthday() {
+        CapersRepository.setupPersistence();
+        CapersRepository.makeDog("test1" , "breed" , 10);
+        Dog tt1 = Dog.fromFile("test1");
+        tt1.haveBirthday();
+        tt1.haveBirthday();
+        tt1.haveBirthday();
+    }
+}
+
+
+ */
+
+
+
+
